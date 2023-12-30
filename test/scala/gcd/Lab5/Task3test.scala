f1da8be28e0b6435e9a9914209f6fbb5b62d3e83
muhammadashabmavia <syedashab80@gmail.com>
2023-12-30 23:35:01 +0500

    new

--- test/scala/gcd/Lab5/Task3test.scala
+++ test/scala/gcd/Lab5/Task3test.scala
@@ -6,8 +6,8 @@ import org.scalatest.FreeSpec
 import chisel3.experimental.BundleLiterals._
 class Task3test extends FreeSpec with ChiselScalatestTester{
     "Chisel "in{
-        test(new eMux(SInt(32.W),SInt(32.W),SInt(32.W))){a=>
-        a.io.inp1.poke(12.S)
+        test(new eMux(UInt(32.W),SInt(32.W),SInt(32.W))){a=>
+        a.io.inp1.poke(12.U)
         a.io.inp2.poke(1.S)
         a.io.sel.poke(true.B)
         a.clock.step(1)
